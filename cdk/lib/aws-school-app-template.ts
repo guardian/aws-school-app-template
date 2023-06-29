23d4ffcaf31b6e8cf666f8d4c01b4a9983fb12c8
Andy Gallagher <fredex42@gmail.com>
2023-06-30 00:07:59 +0100

    defined the app

--- cdk/lib/aws-school-app-template.ts
+++ cdk/lib/aws-school-app-template.ts
@@ -1,11 +1,39 @@
-import { join } from "path";
-import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
-import { GuStack } from "@guardian/cdk/lib/constructs/core";
-import type { App } from "aws-cdk-lib";
-import { CfnInclude } from "aws-cdk-lib/cloudformation-include";
+import type {GuStackProps} from "@guardian/cdk/lib/constructs/core";
+import {GuStack} from "@guardian/cdk/lib/constructs/core";
+import type {App} from "aws-cdk-lib";
+import {GuPlayApp} from "@guardian/cdk";
+import {AccessScope} from "@guardian/cdk/lib/constants";
+import {InstanceClass, InstanceSize, InstanceType} from "aws-cdk-lib/aws-ec2";
 
 export class AwsSchoolAppTemplate extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
+
+    new GuPlayApp(this, {
+      access: {
+        scope: AccessScope.PUBLIC,
+      },
+      app: "aws-school-app-template",
+      applicationLogging: {
+        enabled: true,
+      },
+      certificateProps: {
+        domainName: this.stage==="CODE" ? "simple-example.code.dev-gutools.co.uk" : "simple-example.gutools.co.uk",
+      },
+      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.NANO),
+      monitoringConfiguration: {
+        noMonitoring: true,
+      },
+      scaling: {
+        minimumInstances: 2,
+        maximumInstances: 4
+      },
+      userData: {
+        distributable: {
+          fileName: "aws-school-app-template_1.0_all.deb",
+          executionStatement: "dpkg -i aws-school-app-template/aws-school-app-template_1.0_all.deb"
+        }
+      }
+    })
   }
 }
