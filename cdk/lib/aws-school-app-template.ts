ff756cbca067c32816bcf359bdf8a3fdc87874a0
Andy Gallagher <fredex42@gmail.com>
2023-07-11 10:23:38 +0100

    add NS1 domain record

--- cdk/lib/aws-school-app-template.ts
+++ cdk/lib/aws-school-app-template.ts
@@ -4,21 +4,26 @@ import type {App} from "aws-cdk-lib";
 import {GuPlayApp} from "@guardian/cdk";
 import {AccessScope} from "@guardian/cdk/lib/constants";
 import {InstanceClass, InstanceSize, InstanceType} from "aws-cdk-lib/aws-ec2";
+import {GuCname} from "@guardian/cdk/lib/constructs/dns";
+import {Duration} from "aws-cdk-lib";
 
 export class AwsSchoolAppTemplate extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
 
-    new GuPlayApp(this, {
+    const domainName = this.stage==="CODE" ? "simple-example.code.dev-gutools.co.uk" : "simple-example.gutools.co.uk";
+    const app = "aws-school-app-template";
+
+    const stack = new GuPlayApp(this, {
       access: {
         scope: AccessScope.PUBLIC,
       },
-      app: "aws-school-app-template",
+      app,
       applicationLogging: {
         enabled: true,
       },
       certificateProps: {
-        domainName: this.stage==="CODE" ? "simple-example.code.dev-gutools.co.uk" : "simple-example.gutools.co.uk",
+        domainName,
       },
       instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.NANO),
       monitoringConfiguration: {
@@ -34,6 +39,13 @@ export class AwsSchoolAppTemplate extends GuStack {
           executionStatement: "dpkg -i aws-school-app-template/aws-school-app-template_1.0_all.deb"
         }
       }
-    })
+    });
+
+    new GuCname(this, "dnsName", {
+      domainName,
+      resourceRecord: stack.loadBalancer.loadBalancerDnsName,
+      app,
+      ttl: Duration.minutes(20)
+    });
   }
 }
